50f0fa5d8180d94f3df60d2190c9202340514fe6
baptistecdr <baptistecdr@users.noreply.github.com>
2020-01-01 16:22:34 +0100

    Updated project

--- README.md
+++ README.md
@@ -4,25 +4,28 @@
   <p align="center">
     An open-source firmware for the ESP8266. Execute payload on your PS4 5.05.
     <br>
-    <a href="https://github.com/baptistecdr/ps4/issues/new">Report bug</a>
+    <a href="https://github.com/baptistecdr/PS4-OpenESPHost/issues/new">Report bug</a>
     ·
-    <a href="https://github.com/baptistecdr/ps4/issues/new">Request feature</a>
+    <a href="https://github.com/baptistecdr/PS4-OpenESPHost/new">Request feature</a>
   </p>
+  <img src="https://i.snag.gy/NmokAf.jpg"/>
 </p>
 
 ## Table of contents
 
 - [Quick start](#quick-start)
 - [What's included](#whats-included)
+- [How to build](#how-to-build)
 - [Bugs and feature requests](#bugs-and-feature-requests)
 - [Contributing](#contributing)
 - [Contibutors](#contributors)
+- [Thanks to](#thanks-to)
 
 ## Quick start
 - Download the latest release
 - Flash your ESP8266 with [NodeMCU PyFlasher](https://github.com/marcelstoer/nodemcu-pyflasher/releases)
-- Connect your PS4 to the Wi-Fi (PS4_JAILBREAK)
-- Access to a page via the navigator
+- Connect your PS4 to the Wi-Fi ([PS4_JAILBREAK](https://github.com/baptistecdr/PS4-OpenESPHost/blob/1c338694e28d1f92cd61d42bff66cbf41c0d737f/www/settings.json#L2))
+- Access to a page via the navigator (or type [10.10.10.1](https://github.com/baptistecdr/PS4-OpenESPHost/blob/1c338694e28d1f92cd61d42bff66cbf41c0d737f/www/settings.json#L4))
 - Enjoy !
 
 ## What's included
@@ -34,10 +37,32 @@
 - VV1LD/Stooged: Kernel Dumper
 - Stooged: App2USB
 - Stooged: DB_SG_Backup
+- Stooged: Cache Install
+- Stooged: FS Dumper
+- LightningMods: FTP RW
+- Zer0xFF: React PSPlus
+- ValentinBreiz: Linux Loader
+- MiraDevTeam: MiraFW
+- Stooged: DB_Restore
+- Stooged: DiscDump
+- SiSTR0: Remote Play Enabler
+
+## How to build
+
+- Install [Visual Studio Code](https://code.visualstudio.com)
+- Install [PlatformIO IDE](https://platformio.org)
+- Clone the project
+- Install Gulp globally
+- Install project dependencies
+- Install Node dependencies
+- PlatformIO Build
+- PlatformIO Upload
+- Execute 'gulp' command
+- Upload SPIFFS image
 
 ## Bugs and feature requests
 
-Have a bug or a feature request? Please first search for existing and closed issues. If your problem or idea is not addressed yet, [please open a new issue](https://github.com/baptistecdr/ps4/issues/new).
+Have a bug or a feature request? Please first search for existing and closed issues. If your problem or idea is not addressed yet, [please open a new issue](https://github.com/baptistecdr/PS4-OpenESPHost/issues/new).
 
 ## Contributing
 
@@ -47,10 +72,17 @@ Contributions are welcome!
 
 ## Thanks to
 
+- CTurt
 - Flatz
 - idc
 - Joon
 - qwertyuiop
 - xVortex
 - Stooged
+- Theorywrong
+- ValentinBreiz
+- LightningMods
+- Zer0xFF
+- SiSTR0
+- [Tinkerman](http://tinkerman.cat/optimizing-files-for-spiffs-with-gulp/)
 - Anonymous
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <ESP8266WebServer.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 #include <DNSServer.h>
 #include <ArduinoJson.h>
 #include <FS.h>
-// #include <string.h>
 
 struct Configuration
 {
-    char ssid[31];
-    char password[63];
+    const char *ssid;
+    const char *password;
     IPAddress ip_address;
     IPAddress gateway;
     IPAddress subnet;
@@ -19,94 +19,205 @@ struct Configuration
 const char *DEFAULT_CONFIG_FILENAME = "/settings.json";
 
 /* WEB SERVER CONFIG */
-int DEFAULT_HTTP_PORT = 80;
+ushort DEFAULT_HTTP_PORT = 80;
 
 /* DNS CONFIG */
-int DEFAULT_DNS_PORT = 53;
+ushort DEFAULT_DNS_PORT = 53;
 int DEFAULT_DNS_TTL = 86400; // 24 hours
 
 Configuration config;
-ESP8266WebServer webServer(DEFAULT_HTTP_PORT);
+AsyncWebServer webServer(DEFAULT_HTTP_PORT);
 DNSServer dnsServer;
 
 void loadConfiguration(const char *filename, Configuration &config)
 {
     File file = SPIFFS.open(filename, "r");
-    StaticJsonBuffer<384> jsonBuffer;
-    JsonObject &root = jsonBuffer.parseObject(file);
+    StaticJsonDocument<512> doc;
+    DeserializationError error = deserializeJson(doc, file);
 
-    if (!root.success())
+    if (error) {
         Serial.println("Failed to read file, using default configuration");
+    }
 
     /* Parse the JSON object to the Configuration Structure */
-    strlcpy(config.ssid, root["ssid"], sizeof(config.ssid));
-    strlcpy(config.password, root["password"], sizeof(config.password));
-    config.ip_address.fromString(root["ip_address"].as<char *>());
-    config.subnet.fromString(root["subnet"].as<char *>());
-    config.gateway.fromString(root["gateway"].as<char *>());
+    config.ssid = doc["ssid"].as<const char *>();
+    config.password = doc["password"].as<const char *>();
+    config.ip_address.fromString(doc["ip_address"].as<char *>());
+    config.subnet.fromString(doc["subnet"].as<char *>());
+    config.gateway.fromString(doc["gateway"].as<char *>());
 
-    jsonBuffer.clear();
+    doc.clear();
     file.close();
 }
 
-void serveWebsite()
+void saveConfiguration(const char *filename, const Configuration &config)
 {
-    webServer.serveStatic("/", SPIFFS, "/index.html");
-    /* Serve the entire data folder */
-    webServer.serveStatic("/index.html", SPIFFS, "/index.html");
-    webServer.serveStatic("/css/bootstrap.min.css", SPIFFS, "/css/bootstrap.min.css");
-    webServer.serveStatic("/css/dashboard.min.css", SPIFFS, "/css/dashboard.min.css");
-    webServer.serveStatic("/js/bootstrap.bundle.min.js", SPIFFS, "/js/bootstrap.bundle.min.js");
-    webServer.serveStatic("/js/jquery.min.js", SPIFFS, "/js/jquery.min.js");
-    webServer.serveStatic("/js/jailbreak.min.js", SPIFFS, "/js/jailbreak.min.js");
-    webServer.serveStatic("/payloads/ftp.html", SPIFFS, "/payloads/ftp.html");
-    webServer.serveStatic("/payloads/game-dumper.html", SPIFFS, "/payloads/game-dumper.html");
-    webServer.serveStatic("/payloads/kernel-dumper.html", SPIFFS, "/payloads/kernel-dumper.html");
-    webServer.serveStatic("/payloads/hen.html", SPIFFS, "/payloads/hen.html");
-    webServer.serveStatic("/payloads/enable-vr.html", SPIFFS, "/payloads/enable-vr.html");
-    webServer.serveStatic("/payloads/db-sg-backup.html", SPIFFS, "/payloads/db-sg-backup.html");
-    webServer.serveStatic("/payloads/app-to-usb.html", SPIFFS, "/payloads/app-to-usb.html");
-
-    /* Some how this is not working
-    Dir dir = SPIFFS.openDir("/");
-    while (dir.next())
+    /* Create backup file */
+    String backupFilename = String(filename) + ".bak";
+    SPIFFS.rename(filename, backupFilename);
+    
+    // Opens a file for writing only. Overwrites the file if the file exists. If the file does not exist, creates a new file for writing.
+    File file = SPIFFS.open(filename, "w");
+    if (!file)
     {
-        const char *filename = dir.fileName().c_str();
-        // Exclude configuration file
-        if (!strcmp(DEFAULT_CONFIG_FILENAME, filename)
-            continue;
-        webServer.serveStatic(filename, SPIFFS, filename);
-    } */
+        Serial.println("Failed to create file");
+        // Restore backup file
+        SPIFFS.rename(backupFilename, filename);
+        return;
+    }
+
+   StaticJsonDocument<512> doc;
+
+    /* Set the values */
+    doc["ssid"] = config.ssid;
+    doc["password"] = config.password;
+    doc["ip_address"] = config.ip_address.toString();
+    doc["subnet"] = config.subnet.toString();
+    doc["gateway"] = config.gateway.toString();
+
+    /* Serialize JSON to file */
+    if (serializeJson(doc, file) == 0)
+    {
+        Serial.println("Failed to write to file");
+        // Restore backup file
+        SPIFFS.rename(backupFilename, filename);
+    }
+
+    doc.clear();
+    file.close();
 }
 
 void setup()
 {
     delay(1000);
     Serial.begin(9600);
-    
+
     /* Loading general configuration */
     if (!SPIFFS.begin())
+    {
         Serial.println("Failed to mount SPIFFS");
+    }
+
     loadConfiguration(DEFAULT_CONFIG_FILENAME, config);
-    
+
     /* Settings up Wi-Fi AP */
     WiFi.softAPConfig(config.ip_address, config.gateway, config.subnet);
     WiFi.softAP(config.ssid, config.password);
-    
+
     /* Settings up DNS Server */
     dnsServer.setTTL(DEFAULT_DNS_TTL);
     dnsServer.setErrorReplyCode(DNSReplyCode::ServerFailure);
-    
-     /* Redirect all domains to the local WebServer */
+
+    /* Redirect all domains to the local WebServer */
     dnsServer.start(DEFAULT_DNS_PORT, "*", config.ip_address);
-    
+
     /* Settings up WebServer */
-    serveWebsite();
+    webServer.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
+    webServer.on("/esp8266/restart", HTTP_POST, [](AsyncWebServerRequest *request) {
+        ESP.restart();
+    });
+    webServer.on("/esp8266/reset", HTTP_POST, [](AsyncWebServerRequest *request) {
+        ESP.reset();
+    });
+    webServer.on("/esp8266/information", HTTP_GET, [](AsyncWebServerRequest *request) {
+        DynamicJsonDocument doc(1024);
+
+        /* Set values */
+        doc["boot_mode"] = ESP.getBootMode();
+        doc["boot_version"] = ESP.getBootVersion();
+        doc["chip_id"] = ESP.getChipId();
+        doc["core_version"] = ESP.getCoreVersion();
+        doc["cpu_freq"] = ESP.getCpuFreqMHz();
+        doc["cycle_count"] = ESP.getCycleCount();
+        doc["flash_chip_id"] = ESP.getFlashChipId();
+        doc["flash_chip_mode"] = ESP.getFlashChipMode();
+        doc["flash_chip_real_size"] = ESP.getFlashChipRealSize();
+        doc["flash_chip_size"] = ESP.getFlashChipSize();
+        doc["flash_chip_size_by_chip_id"] = ESP.getFlashChipSizeByChipId();
+        doc["flash_chip_speed"] = ESP.getFlashChipSpeed();
+        doc["free_heap"] = ESP.getFreeHeap();
+        doc["free_sketch_space"] = ESP.getFreeSketchSpace();
+        doc["full_version"] = ESP.getFullVersion();
+        doc["reset_info"] = ESP.getResetInfo();
+        doc["reset_reason"] = ESP.getResetReason();
+        doc["sdk_version"] = ESP.getSdkVersion();
+        doc["sketch_md5"] = ESP.getSketchMD5();
+        doc["sketch_size"] = ESP.getSketchSize();
+        doc["vcc"] = ESP.getVcc();
+        String response;
+        
+        // Serialize JSON to Sring to send it
+        serializeJson(doc, response);
+        doc.clear();
+
+        return request->send(200, "application/json", response);
+    });
+    webServer.on("/settings/update", HTTP_POST, [](AsyncWebServerRequest *request) {
+        /* Validation Rules */
+        if (!request->hasParam("ssid", true))
+        {
+            return request->send(400, "text/plain", "SSID parameter is required");
+        }
+        if (!request->hasParam("password", true))
+        {
+            return request->send(400, "text/plain", "Password parameter is required");
+        }
+        if (!request->hasParam("ip_address", true))
+        {
+            return request->send(400, "text/plain", "IP Address parameter is required");
+        }
+        if (!request->hasParam("subnet", true))
+        {
+            return request->send(400, "text/plain", "Subnet parameter is required");
+        }
+        if (!request->hasParam("gateway", true))
+        {
+            return request->send(400, "text/plain", "Gateway parameter is required");
+        }
+
+        /* If we made it so far, we can modify the configuration and save it */
+        AsyncWebParameter *p = request->getParam("ssid", true);
+        if (p->value().length() == 0)
+        {
+            return request->send(400, "text/plain", "SSID value is required");
+        }
+        config.ssid = p->value().c_str();
+
+        /* No verification for password because Wi-Fi AP can be open */
+        p = request->getParam("password", true);
+        config.password = p->value().c_str();
+
+        p = request->getParam("ip_address", true);
+        if (!config.ip_address.fromString(p->value()))
+        {
+            return request->send(400, "text/plain", "IP Address is not valid");
+        }
+
+        p = request->getParam("subnet", true);
+        if (!config.subnet.fromString(p->value()))
+        {
+            return request->send(400, "text/plain", "Subnet is not valid");
+        }
+
+        p = request->getParam("gateway", true);
+        if (!config.gateway.fromString(p->value()))
+        {
+            return request->send(400, "text/plain", "Gateway is not valid");
+        }
+
+        saveConfiguration(DEFAULT_CONFIG_FILENAME, config);
+        return request->send(200, "text/plain", "Configuration updated");
+    });
+    /* If the user try to access to an unknown page, we redirect him to the root page */
+    webServer.onNotFound([](AsyncWebServerRequest *request){
+        return request->redirect("/");
+    });
+    // Start WebServer
     webServer.begin();
 }
 
 void loop()
 {
+    // Start processing DNS requests
     dnsServer.processNextRequest();
-    webServer.handleClient();
 }
